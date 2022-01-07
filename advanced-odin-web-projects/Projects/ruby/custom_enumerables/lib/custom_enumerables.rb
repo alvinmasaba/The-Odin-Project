7c441274edd2857763429e91d47458b0f1fa270b
funkyman3121 <alvinmasaba@gmail.com>
2022-01-06 23:13:57 -0500

    Created my_each_with_index

--- advanced-odin-web-projects/Projects/ruby/custom_enumerables/lib/custom_enumerables.rb
+++ advanced-odin-web-projects/Projects/ruby/custom_enumerables/lib/custom_enumerables.rb
@@ -2,16 +2,37 @@ module Enumerable
   def my_each # Identical method to #each
     arr = self.instance_of?(Hash) ? self.to_a : self
     i = 0
-    arr.count.times do
-      yield arr[i]
-      i += 1
+    if block_given?
+      arr.count.times do
+        yield arr[i]
+        i += 1
+      end
+    else
+      return self.to_enum 
     end
 
-    return self 
+    self 
   end
+
+  def my_each_with_index
+    arr = self.instance_of?(Hash) ? self.to_a : self
+    i = 0
+    if block_given?
+      arr.count.times do
+        yield arr[i], i
+        i += 1
+      end
+    else
+      return self.to_enum 
+    end
+
+    self 
+  end
+
+  
 end
 
-a = {:a=>1, :b=>2, :c=>3}
 
-a.my_each { |x| puts x*2 }
+
+
 
---
+++ advanced-odin-web-projects/Projects/ruby/custom_enumerables/spec/custom_enumerables_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require './lib/custom_enumerables'
+
+RSpec.describe 'Custom Enumerables' do
+  describe '#my_each' do
+    it 'works with arrays of integers in blocks' do
+      numbers = [1,2,3,4,5]
+      expect(numbers.my_each { |item| puts item }).to eql(numbers.each { |item| puts item })
+    end
+
+    it 'works with arrays of strings in blocks' do
+      numbers = %w[a b c d]
+      expect(numbers.my_each { |item| puts item }).to eql(numbers.each { |item| puts item })
+    end
+
+    it 'works with empty arrays and blocks' do
+      numbers = []
+      expect(numbers.my_each { |item| puts item }).to eql(numbers.each { |item| puts item })
+    end
+
+    it 'works with hashes and blocks' do
+      numbers = { :a => 1, :b => 2, :c => 3, :d => 4 }
+      expect(numbers.my_each { |item| puts item }).to eql(numbers.each { |item| puts item })
+    end
+
+    # it 'works with when there is no block' do
+    #  numbers = [1,2,3,4,5]
+    #  expect(numbers.my_each).to eql(numbers.each)
+    # end
+  end
+end

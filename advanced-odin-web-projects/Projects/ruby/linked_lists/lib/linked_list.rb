d8b79e7234a598205fc67e680ca1bfb4c64cdcc7
funkyman3121 <alvinmasaba@gmail.com>
2022-01-17 22:26:22 -0500

    Added pop, contains, find methods

--- advanced-odin-web-projects/Projects/ruby/linked_lists/lib/linked_list.rb
+++ advanced-odin-web-projects/Projects/ruby/linked_lists/lib/linked_list.rb
@@ -3,17 +3,65 @@
 require_relative './node.rb'
 
 class LinkedList
-  def initialize
-    @head = nil
-    @tail = nil
+  attr_accessor :head, :tail, :nodes
+  
+  def initialize(head = nil, tail = nil)
+    @head = head
+    @tail = tail
+    @nodes = []
   end
 
   def append(value)
-    @tail = Node.new(value)
+    @tail = Node.new(value, nil, @nodes.size)
+    @nodes << @tail
   end
 
   def prepend(value)
-    @head = Node.new(value, @head)
+    @head = Node.new(value, @head, 0)
+
+    # increment the index number of each node except the head
+    @nodes.each { |node| node.index += 1}
+
+    # add head to the front of the list
+    @nodes.unshift(@head)
   end
- 
+
+  def size
+    @nodes.size
+  end
+
+  def head
+    @head.value
+  end
+
+  def tail
+    @tail.value
+  end
+
+  def at(idx)
+    node = @nodes.find { |node| node.index == idx }
+    node.value
+  end
+
+  def pop
+    node = @tail.value
+    @nodes = @nodes[0...-1]
+    @tail = @nodes[-1]
+    node
+  end
+
+  def contains?(value)
+    @nodes.any? { |node| node.value == value }
+  end
+
+  def find(value)
+    result = nil
+    @nodes.each { |node| node.index == value ? node.value : result }
+  end
+
+  def to_s
+    string = ""
+    @nodes.each do |node|
+      string += ""
+
 end
--- advanced-odin-web-projects/Projects/ruby/linked_lists/lib/node.rb
+++ advanced-odin-web-projects/Projects/ruby/linked_lists/lib/node.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Node
-  attr_accessor :value, :next_node
-  
-  def initialize(value=nil, next_node=nil)
+  attr_accessor :value, :next_node, :index
+
+  def initialize(value=nil, next_node=nil, index)
     @value = value
     @next_node = next_node
+    @index = index
   end
 end

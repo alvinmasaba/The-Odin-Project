520d1949f84e11d1b73329aeb338f24f3c5536b2
funkyman3121 <alvinmasaba@gmail.com>
2022-01-18 21:41:27 -0500

    Refactored linkedlist class to use pointers rather than an array.

--- advanced-odin-web-projects/Projects/ruby/linked_lists/lib/linked_list.rb
+++ advanced-odin-web-projects/Projects/ruby/linked_lists/lib/linked_list.rb
@@ -1,112 +1,154 @@
 # frozen_string_literal: true
 
 require_relative './node'
+require 'pry'
 
 # Imitates the functionality of a traditional linked list
 class LinkedList
-  attr_accessor :head, :tail, :nodes
+  attr_accessor :head, :tail
 
-  def initialize(nodes = [])
-    if nodes.empty?
-      @head = nil
-      @tail = nil
-      @nodes = nodes
-    else
-      @head = nodes[0]
-      @tail = nodes[-1]
-      @nodes = []
-      nodes.each_with_index { |value, i| @nodes << Node.new(value, nodes[i + 1]) }
-    end
+  def initialize
+    @head = nil
+    @tail = nil
   end
 
   def append(value)
-    if @nodes.empty?
-      @nodes << Node.new(value, nil)
-      @head = value
+    # If there is no head, then create a new node, set it as the head, and point it to nil
+    if @head.nil?
+      @head = Node.new(value, nil)
+
+    # If there is no tail, create a new node, set it as the tail, point to nil, and set the head to point to it
     elsif @tail.nil?
-      @nodes << Node.new(value, nil)
-      @nodes[0].next_node = value
+      @tail = Node.new(value, nil)
+      @head.next_node = @tail
+
+    # Otherwise, create a new node, make the tail point to it, and set it is the tail
     else
-      # Make the previous tail point to the value to be appended
-      @nodes[-1].next_node = value
-      # Append the new value as a node instance to the list of nodes with value 'value' and next_node 'nil'
-      @nodes << Node.new(value, nil)
+      new_tail = Node.new(value, nil)
+      @tail.next_node = new_tail
+      @tail = new_tail
     end
-    # Set @tail to value
-    @tail = value
-    @nodes.to_s
   end
 
   def prepend(value)
-    if @nodes.empty?
-      # Add a new node to the front of the list with value 'value' and next_node 'nil'
-      @nodes.unshift(Node.new(value))
-    else
-      @nodes.unshift(Node.new(value, @head))
-    end
+    # If there is no head, then create a new node, set it as the head, and point it to nil
+    if @head.nil?
+      @head = Node.new(value, nil)
 
-    @head = value
-    @nodes.to_s
+    # If there is no tail, create a new node and point it to the current head, make the current head the tail,
+    # then set the new node to head
+    elsif @tail.nil?
+      new_head = Node.new(value, @head)
+      @tail = @head
+      @head = new_head
+
+    # Otherwise, simply make the new node the head
+    else
+      new_head = Node.new(value, @head)
+      @head = new_head
+    end
   end
 
   def size
-    @nodes.size
+    i = 0
+    current_node = @head
+
+    # Increment i until the end of the list (nil) is reached, then return i
+    until current_node.nil?
+      current_node = current_node.next_node
+      i += 1
+    end
+
+    i
   end
 
   def at(index)
-    @nodes[index].value
+    i = 0
+    current_node = @head
+
+    # Move to the next_node #index times. Then return that node.
+    until i == index
+      current_node = current_node.next_node
+      i += 1
+    end
+
+    current_node.value
   end
 
   def pop
-    node = @tail
-    @nodes = @nodes[0...-1]
-    @tail = @nodes[-1].value
-    @nodes[-1].next_node = nil
-    node
+    popped_node = @tail
+    current_node = @head
+
+    # Move to the next_node until the penultimate node is reached, make that node the tail, return the previous tail.
+    current_node = current_node.next_node until current_node.next_node == @tail
+    current_node.next_node = nil
+    @tail = current_node
+    popped_node.value
   end
 
   def contains?(value)
-    @nodes.any? { |node| node.value == value }
+    current_node = @head
+    result = false
+
+    # Starting at the head, check each node to see if its value == given value, if so, toggle result to true.
+    until current_node.next_node.nil?
+      result = current_node.value == value ? true : result
+      current_node = current_node.next_node
+    end
+
+    result
   end
 
   def find(value)
-    result = nil
-    @nodes.each_with_index { |node, i| node.value == value ? result = i : result }
+    i = 0
+    current_node = @head
+    result = puts "#{value} is not in the linked list."
+
+    until current_node.next_node.nil?
+      if current_node.value == value
+        result = i
+        current_node = @tail
+      end
+
+      current_node = current_node.next_node
+      i += 1
+    end
+
     result
   end
 
   def to_s
     string = ''
-    @nodes.each do |node|
-      string += "( #{node.value} ) -> "
+    current_node = @head
+
+    until current_node.next_node.nil?
+      string += "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
     end
-    string += 'nil'
+
+    string.concat('nil')
   end
 
   def insert_at(value, index)
-    index = index > @nodes.size ? @nodes.size : index
-
+    # Create a new node that points to the node already at index. Unless index is 0, set the preceding node to point
+    # to the new node.
     if index.zero?
       prepend(value)
+    elsif index == size
+      append(value)
     else
-      @nodes.insert(Node.new(value, @nodes[index].value))
-      @nodes[index - 1].next_node = value
+      new_node = Node.new(value, at(index))
+      at(index - 1).next_node = new_node
     end
-
-    @nodes.to_s
   end
 
   def remove_at(index)
-    if index == @nodes.size - 1
+    if index == size - 1
       pop
     elsif index.zero?
-      @nodes = @nodes[1..-1]
-      @head = @nodes[0].value
+      @head = @head.next_node
     else
-      @nodes[index - 1].next_node = @nodes[index + 1].value
-      @nodes = @nodes.slice(index)
+      at(index - 1).next_node = at(index + 1) 
     end
-
-    @nodes.to_s
   end
 end

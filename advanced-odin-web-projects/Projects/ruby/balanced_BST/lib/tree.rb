52861b565da501da95514556ec58e79de8b5338c
funkyman3121 <alvinmasaba@gmail.com>
2022-01-25 13:46:59 -0500

    Refactored tree class

--- advanced-odin-web-projects/Projects/ruby/balanced_BST/lib/tree.rb
+++ advanced-odin-web-projects/Projects/ruby/balanced_BST/lib/tree.rb
@@ -4,17 +4,21 @@ class Tree
   attr_accessor :root, :array
 
   def initialize(array)
-    @array = array.sort
+    @array = array.sort.uniq
+    @root = self.build_tree
   end
 
-  def build_tree(array = @array, start = 0, tail = @array.size - 1)
-    return nil if start > tail
+  def build_tree(array = @array, start = 0, tail = array.size - 1)
+    if start > tail
+      return nil 
+    end
+
     mid = (start + tail) / 2
-    @root = Node.new(array[mid])
-    
-    @root.left_child = build_tree(array, start, mid - 1)
-    @root.right_child = build_tree(array, mid + 1, tail)
-    @root
+    root = Node.new(array[mid])
+    root.left_child = build_tree(array, start, mid - 1)
+    root.right_child = build_tree(array, mid + 1, tail)
+
+    root
   end
 end
 

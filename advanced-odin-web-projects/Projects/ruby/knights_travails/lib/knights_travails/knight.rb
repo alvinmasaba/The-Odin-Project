3653ebe6f3f309bcaefc8346f9cc84574686fac6
funkyman3121 <alvinmasaba@gmail.com>
2022-02-01 19:16:43 -0500

    Updated knight.rb

--- advanced-odin-web-projects/Projects/ruby/knights_travails/lib/knights_travails/knight.rb
+++ advanced-odin-web-projects/Projects/ruby/knights_travails/lib/knights_travails/knight.rb
@@ -2,7 +2,7 @@
 
 # Class to create knight object
 class Knight
-  attr_accessor :position, :moves
+  attr_accessor :position
 
   def initialize(position)
     if position.all? { |n| n.between?(1, 8) && position.size == 2 } 
@@ -15,24 +15,35 @@ class Knight
     @moves = find_moves
   end
 
-  def find_moves
+  # A function that builds a graph of all the moves a knight can make
+  # starting from its position.
+  def build_move_tree(root = @position)
+    return if root == @position
+
+
+
+
+
+
+
+  def find_moves(position)
     moves = []
 
     # Push to moves [position[0] - 1, position[1] +/- 2]
-    moves << [@position[0] - 1, @position[1] + 2]
-    moves << [@position[0] - 1, @position[1] - 2]
+    moves << [position[0] - 1, position[1] + 2]
+    moves << [position[0] - 1, position[1] - 2]
 
     # Push to moves [position[0] + 1, position[1] +/- 2]
-    moves << [@position[0] + 1, @position[1] + 2]
-    moves << [@position[0] + 1, @position[1] - 2]
+    moves << [position[0] + 1, position[1] + 2]
+    moves << [position[0] + 1, position[1] - 2]
 
     # Push to moves [position[0] - 2, position[1] +/- 1]
-    moves << [@position[0] - 2, @position[1] + 1]
-    moves << [@position[0] - 2, @position[1] - 1]
+    moves << [position[0] - 2, position[1] + 1]
+    moves << [position[0] - 2, position[1] - 1]
 
     # Push to moves [position[0] + 2, position[1] +/- 1]
-    moves << [@position[0] + 2, @position[1] + 1]
-    moves << [@position[0] + 2, @position[1] - 1]
+    moves << [position[0] + 2, position[1] + 1]
+    moves << [position[0] + 2, position[1] - 1]
 
     moves.reject { |move| !move.between?
   end

0a5125d9a428687a383ce3cb8a0a6944adca727f
funkyman3121 <alvinmasaba@gmail.com>
2022-02-04 15:54:48 -0500

    Added tests to caesar_cipher

--- advanced-odin-web-projects/Exercises/Back_End/ruby/ruby_scripts/caesar_cipher/spec/caesar_spec.rb
+++ advanced-odin-web-projects/Exercises/Back_End/ruby/ruby_scripts/caesar_cipher/spec/caesar_spec.rb
@@ -1,1 +1,31 @@
+require './lib/caesar_cipher.rb'
 
+describe '#caesar_cipher' do
+  it 'works with a spaceless string' do
+    expect(caesar_cipher('whatastring', 5)).to eql('bmfyfxywnsl')
+  end
+
+  it 'works with spaces' do
+    expect(caesar_cipher('what a string', 5)).to eql('bmfy f xywnsl')
+  end
+
+  it 'works with punctuation' do
+    expect(caesar_cipher('what a string!', 5)).to eql('bmfy f xywnsl!')
+  end
+
+  it 'is case sensitive' do
+    expect(caesar_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+  end
+
+  it 'works when the factor is negative' do
+    expect(caesar_cipher('What a string!', -1)).to eql('Vgzs z rsqhmf!')
+  end
+
+  it 'works when the factor is zero' do
+    expect(caesar_cipher('What a string!', 0)).to eql('What a string!')
+  end
+
+  it 'works when the factor is greater than 26' do
+    expect(caesar_cipher('What a string!', 31)).to eql('Bmfy f xywnsl!')
+  end
+end

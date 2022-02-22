6fa3b659f9019adc589f0b0ec56c04e342f8c507
funkyman3121 <alvinmasaba@gmail.com>
2022-02-22 16:19:31 -0500

    Tests for initialize and #drop_marker

--- advanced-odin-web-projects/Projects/ruby/connect_four/lib/connect_four/game.rb
+++ advanced-odin-web-projects/Projects/ruby/connect_four/lib/connect_four/game.rb
@@ -6,9 +6,20 @@ require_relative 'player'
 class Game
   attr_accessor :player1, :player2, :board
 
-  def initialize(player1 = Player.new('Player 1'), player2 = Player.new('Player 2'), board = nil)
-    @player1 = player1
-    @player2 = player2
-    @board = board
+  def initialize(rows = 6, cols = 7)
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+    @board = Array.new(rows) { Array.new(cols) { |_n| '#' } }
+  end
+
+  def drop_marker(marker, column)
+    # Starting in reverse order, change the first empty space ('#')
+    # to the marker, then break.
+    board.reverse_each do |row|
+      next unless row[column] == '#'
+      
+      row[column] = marker
+      break
+    end
   end
 end
--- advanced-odin-web-projects/Projects/ruby/connect_four/spec/connect_four_spec.rb
+++ advanced-odin-web-projects/Projects/ruby/connect_four/spec/connect_four_spec.rb
@@ -1,11 +1,68 @@
 # frozen_string_literal: true
 
-require 'lib/connect_four/game'
-require 'lib/connect_four/player'
+require './lib/connect_four/game'
+require './lib/connect_four/player'
 
 describe Game do
-  subject(:game_board) { described_class.new }
-  # Creates a board of a specified size
+  subject(:game_board) { described_class.new(9, 7) }
+
+  describe '#initialize' do
+    # Creates a board of a specified size.
+    it 'has 9 rows' do
+      expect(game_board.board.size).to eql(9)
+    end
+
+    it 'has 7 spaces in each row' do
+      board = game_board.board
+      result = board.all? { |row| row.size == 7 }
+      expect(result).to be(true)
+    end
+  end
+
+  describe '#drop_marker' do
+    # When drop_marker is called, a player picks a column to place
+    # their symbol.
+    marker = "\u2600".encode('utf-8')
+
+    context 'when the column is empty' do
+      subject(:empty_col) { described_class.new(6, 7) }
+
+      it 'places the marker in the lowest row of the chosen column' do
+        column = 3
+        empty_col.drop_marker(marker, column)
+        result = empty_col.board[-1][column]
+        expect(result).to eql(marker)
+      end
+    end
+
+    context 'when there are 2 makers already in the column' do
+      subject(:nonempty_col) {described_class.new(6, 7) }
+
+      it 'places the marker in the 3rd lowest row' do
+        column = 3
+        nonempty_col.board[-1][3] = marker
+        nonempty_col.board[-2][3] = marker
+
+        nonempty_col.drop_marker(marker, column)
+        result = nonempty_col.board[-3][column]
+        expect(result).to eql(marker)
+      end
+    end
+
+    context 'when column is full' do
+      subject(:full_col) {described_class.new(6, 7) }
+      other_marker = 'O'
+
+      it 'does not place the marker in the column' do
+        column = 3
+        full_col.board.each { |row| row[column] = other_marker }
+        full_col.drop_marker(marker, column)
+
+        result = full_col.board.any? { |row| row[column] == marker }
+        expect(result).to be(false) and expect(full_col.board[0][column]).to eql(other_marker)
+      end
+    end
+  end
 end
 
 describe Player do
